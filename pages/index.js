b324641f04485179e54353e2dcaa38afcdf1cb29
Namrata0407 <namrataawasthi077@gmail.com>
2023-08-19 14:26:48 +0530

    textmessage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import TextMessage from '@/Components/text'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-     jhjhgjhjgh
+     <TextMessage/>
     </>
   )
 }
