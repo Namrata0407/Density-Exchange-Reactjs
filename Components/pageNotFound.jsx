1fd18307e46d69ce3f463977ac9c2ce28bf41a33
Namrata0407 <namrataawasthi077@gmail.com>
2023-08-19 13:13:51 +0530

    navbar

---
+++ Components/pageNotFound.jsx
@@ -0,0 +1,13 @@
+import {Heading} from "@chakra-ui/react"
+
+const PageNotFound = ()=>{
+    return(
+        <div>
+            <Heading>
+            Since this is an assignment, only one page is working, as everything is just for demo purposes.     
+           </Heading>
+        </div>
+    )
+};
+
+export default PageNotFound;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Navbar from '@/Components/navbar';
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<ChakraProvider>
+  <Navbar/>
+<Component {...pageProps} />
+</ChakraProvider>
+ 
+  )
 }
